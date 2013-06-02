fe1bea1f74bc6bc4dbef54d44612081db44c2444
Tomás Pollak <tomaspollak@gmail.com>
2013-06-02 13:53:22 -0400

    Support ActionMailer 3.

--- lib/tuktuk/rails.rb
+++ lib/tuktuk/rails.rb
@@ -2,13 +2,27 @@ if ActionMailer::Base.respond_to?(:add_delivery_method)
 	
 	ActionMailer::Base.add_delivery_method :tuktuk, Tuktuk
 
+	module Tuktuk
+
+		def self.deliver!(mail)
+			@logger = Rails.logger unless config[:log_to]
+			resp, email = deliver(mail)
+			raise resp if resp.is_a?(Exception)
+		end
+
+		def self.new(options = {})
+			options = options
+			self
+		end
+
+	end
+
 else
 
 	class ActionMailer::Base
 
 		def perform_delivery_tuktuk(mail)
-			resp, email = Tuktuk.deliver(mail)
-			raise resp if resp.is_a?(Exception)
+			Tuktuk.deliver!(mail)
 	  end
 
 	end
