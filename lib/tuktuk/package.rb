ba4f94738a50a5b56d15d740607b32c12ccbb399
Tomás Pollak <tomaspollak@gmail.com>
2013-08-01 02:37:34 -0400

    Support for attachments.

--- lib/tuktuk/package.rb
+++ lib/tuktuk/package.rb
@@ -10,25 +10,31 @@ module Package
 
   class << self
 	
-		def build(message, index = nil)
-			mail = message.is_a?(Hash) ? new(message) : message.is_a?(Mail) ? message : Mail.read_from_string(message.to_s)
-			mail.array_index = index if index
-			mail
-		end
+    def build(message, index = nil)
+      mail = message.is_a?(Hash) ? new(message) : message.is_a?(Mail) ? message : Mail.read_from_string(message.to_s)
+      mail.array_index = index if index
+      mail
+    end
 
     def new(message)
       mail = message[:html_body] ? mixed(message) : plain(message)
       mail.charset = 'UTF-8'
 
-      mail['In-Reply-To'] = message[:in_reply_to] if message[:in_reply_to]
-      mail['List-Archive'] = message[:list_archive] if message[:list_archive]
-      mail['List-Id'] = message[:list_id] if message[:list_id]
-      mail['X-Mailer'] = "Tuktuk SMTP v#{Tuktuk::VERSION}"
+      mail['In-Reply-To']   = message[:in_reply_to] if message[:in_reply_to]
+      mail['List-Archive']  = message[:list_archive] if message[:list_archive]
+      mail['List-Id']       = message[:list_id] if message[:list_id]
+      mail['X-Mailer']      = "Tuktuk SMTP v#{Tuktuk::VERSION}"
 
       if message[:return_path]
         mail['Return-Path'] = message[:return_path]
         mail['Bounces-To']  = message[:return_path]
         mail['Errors-To']   = message[:return_path]
+      end
+      
+      if message[:attachments].any?
+        message[:attachments].each do |file|
+          mail.add_file(file)
+        end
       end
 
       mail
